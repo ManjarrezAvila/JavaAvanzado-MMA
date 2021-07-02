46586f52409d32b298cecbcb02dff7f952583aec
Manjarrez Avila <m.manjarrez.avila@accenture.com>
2021-07-02 14:34:01 -0500

    Exercise 1

---
+++ Figure.java
@@ -0,0 +1,16 @@
+public abstract class Figure {
+
+    public abstract void draw();
+
+    public class Triangle extends Figure {
+
+        @Override
+        public void draw() {
+            /*logic to draw 
+            a triangle */
+
+        }
+    }
+
+
+}
